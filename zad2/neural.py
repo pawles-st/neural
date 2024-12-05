f61a40eb86a0a16483dbd33011687f30227cc254
Paweł Leśniak <268419@student.pwr.edu.pl>
2024-12-05 19:21:32 +0100

    Fix digits network

--- zad2/neural.py
+++ zad2/neural.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import math
 import numpy as np
 
@@ -11,10 +12,10 @@ def tanh(x: np.ndarray) -> np.ndarray:
     return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
 def d_tanh(y: np.ndarray) -> np.ndarray:
-    return 1 - y^2
+    return 1 - np.square(y)
 
 def relu(x: np.ndarray) -> np.ndarray:
-    return np.max(0, x)
+    return np.maximum(0, x)
 
 def d_relu(y: np.ndarray) -> np.ndarray:
     return np.where(y > 0, 1, 0)
@@ -29,14 +30,15 @@ def softmax(x: np.ndarray) -> np.ndarray:
     exp_x = np.exp(x)
     return exp_x / np.sum(exp_x)
 
-def d_mse(result: np.ndarray, expected: np.ndarray) -> np.ndarray:
-    pass
-
-def d_cce(result: np.ndarray, expected: np.ndarray) -> np.ndarray:
-    pass
+class Activation(Enum):
+    SIGMOID = 1
+    TANH = 2
+    RELU = 3
+    LEAKY_RELU = 4
+    SOFTMAX = 5
 
 class Layer:
-    def __init__(self: 'Layer', input_size: int, layer_size: int, learning_rate: float):
+    def __init__(self: 'Layer', input_size: int, layer_size: int, activation: Activation, learning_rate: float):
         """
         Initialise the network layer
 
@@ -44,8 +46,27 @@ class Layer:
         input_size (int): The size of the input vector for this layer
         layer_size (int): The number of neurons this layer has
         learning_rate (float): A scaling factor for gradient descent during back propagation
+        activation (Activation): The activation function for this layer
         """
         self.learning_rate = learning_rate
+
+        if activation == Activation.SIGMOID:
+            self.activation = sigmoid
+            self.d_activation = d_sigmoid
+        elif activation == Activation.TANH:
+            self.activation = tanh
+            self.d_activation = d_tanh
+        elif activation == Activation.RELU:
+            self.activation = relu
+            self.d_activation = d_relu
+        elif activation == Activation.LEAKY_RELU:
+            self.activation = leaky_relu
+            self.d_activation = d_leaky_relu
+        elif activation == Activation.SOFTMAX:
+            self.activation = softmax
+            self.d_activation = None
+        else:
+            raise ValueError("Invalid activation function")
 
         self.input_size = input_size
         input_size_with_bias = input_size + 1
@@ -74,30 +95,67 @@ class Layer:
         d_loss (np.ndarray): The derivatives of the loss function by each of the outputs of
                              this layer; should have shape (self.layer_size, 1)
         """
+        # the argument in the mathematical sense is self.net,
+        # but the derivative is dependent only on self.result
         delta = d_loss * self.__d_activate(self.result)
         d_weights = delta @ self.data_with_bias.T
+        # print("weights:", self.weights)
+        # print("delta:", delta)
+        # print("biasd:", self.data_with_bias.T)
+        # print("d:", d_weights)
 
         self.weights -= self.learning_rate * d_weights
+        # print("weights:", self.weights)
+        # print("---")
+
+        return (self.weights.T @ delta)[1:]
+
+    def backward_propagate_softmax(self: 'Layer', result: np.ndarray, expected: np.ndarray) -> np.ndarray:
+        """
+        Modify the weights based on the categorical cross entropy function and softmax on the last layer
+
+        Parameters:
+        result (np.ndarray): The result of the layer for the inputs
+        expected (np.ndarray): The expected result for the inputs
+        """
+        # print(result)
+        # print(expected)
+        delta = result - expected
+        d_weights = delta @ self.data_with_bias.T
+        # print("weights:", self.weights)
+        # print("delta:", delta)
+        # print("biasd:", self.data_with_bias.T)
+        # print("d:", d_weights)
+
+        self.weights -= self.learning_rate * d_weights
+        # print("weights:", self.weights)
+        # print("---")
 
         return (self.weights.T @ delta)[1:]
 
     def __activate(self: 'Layer', net: np.ndarray) -> np.ndarray:
-        return sigmoid(net)
+        return self.activation(net)
 
-    def __d_activate(self: 'Layer', net: np.ndarray) -> np.ndarray:
-        return d_sigmoid(net)
+    def __d_activate(self: 'Layer', result: np.ndarray) -> np.ndarray:
+        return self.d_activation(result)
 
 class NeuralNetwork:
-    def __init__(self: 'NeuralNetwork', input_size: int, layers: int, neurons: np.ndarray, learning_rate = 1/100):
+    def __init__(self: 'NeuralNetwork', input_size: int, no_layers: int, no_neurons: np.ndarray, activations = np.ndarray, learning_rate = 0.01):
         self.layers = [
-            Layer(input_size, neurons[0], learning_rate) if i == 0
-            else Layer(neurons[i - 1], neurons[i], learning_rate)
-            for i in range(layers)
+            Layer(input_size, no_neurons[0], activations[0], learning_rate) if i == 0
+            else Layer(no_neurons[i - 1], no_neurons[i], activations[i], learning_rate)
+            for i in range(no_layers)
         ]
+
+        if activations[-1] == Activation.SOFTMAX:
+            self.__backward_propagate_last = self.__backward_propagate_cce
+        else:
+            self.__backward_propagate_last = self.__backward_propagate_mse
 
     def fit(self: 'NeuralNetwork', data: np.ndarray, expected: np.ndarray):
         result = self.__forward_propagate(data)
-        self.__backward_propagate(result, expected)
+        d_loss = self.__backward_propagate_last(result, expected)
+        self.__backward_propagate(d_loss)
 
     def predict(self: 'NeuralNetwork', data: np.ndarray) -> np.ndarray:
         return self.__forward_propagate(data)
@@ -109,58 +167,13 @@ class NeuralNetwork:
 
         return result
 
-    def __backward_propagate(self: 'NeuralNetwork', result: np.ndarray, expected: np.ndarray):
-        d_loss = result - expected
-        for layer in reversed(self.layers):
+    def __backward_propagate(self: 'NeuralNetwork', d_loss: np.ndarray):
+        for layer in reversed(self.layers[:-1]):
             d_loss = layer.backward_propagate(d_loss)
 
-# nn = NeuralNetwork(1, 3, [3, 3, 1])
+    def __backward_propagate_mse(self: 'NeuralNetwork', result: np.ndarray, expected: np.ndarray) -> np.ndarray:
+        d_loss = result - expected
+        return self.layers[-1].backward_propagate(d_loss)
 
-# fizzbuzz_data = [
-    # (i, 3) if i % 3 == 0 and i % 5 == 0 else
-    # (i, 2) if i % 5 == 0 else
-    # (i, 1) if i % 3 == 0 else
-    # (i, 0)
-    # for i in range(100000)
-# ]
-# fizzbuzz_test = [
-    # (i, 3) if i % 3 == 0 and i % 5 == 0 else
-    # (i, 2) if i % 5 == 0 else
-    # (i, 1) if i % 3 == 0 else
-    # (i, 0)
-    # for i in range(150000, 160000)
-# ]
-# for i in range(10):
-    # for data in fizzbuzz_data:
-        # (d, e) = data
-        # if d % 10000 == 0:
-            # print(str(d) + "...")
-        # nn.fit(d / 4, e)
-
-# errors = 0
-
-# for data in fizzbuzz_test:
-    # (d, e) = data
-    # r = nn.predict(d / 4)
-    # print(d, "- got", r, ", expected", e)
-    # if np.linalg.norm(r - e) > 0.1:
-        # errors += 1
-
-# print("errors:", errors / 10000)
-
-# layer = Layer(3, 3, 1)
-# print("weights:", layer.weights)
-
-# data = np.array([[1/3], [2/3], [1]])
-# expected = np.array([[4/6], [5/6], [1]])
-
-# for _ in range(1, 1000):
-    # result = layer.forward_propagate(data)
-    # print("result: ", result)
-    # d_loss = result - expected
-    # print("d_loss: ", d_loss)
-    # layer.backward_propagate(d_loss)
-    # print("new weights:", layer.weights)
-
-# result = layer.forward_propagate(data)
-# print("final: ", result)
+    def __backward_propagate_cce(self: 'NeuralNetwork', result: np.ndarray, expected: np.ndarray) -> np.ndarray:
+        return self.layers[-1].backward_propagate_softmax(result, expected)
--- zad2/test.py
+++ zad2/test.py
@@ -1,25 +1,82 @@
-from neural import NeuralNetwork
+from neural import Activation, Layer, NeuralNetwork
 import numpy as np
 import mnist_loader
 
-(training_data, validation_data, test_data) = mnist_loader.load_data_wrapper()
+# (training_data, validation_data, test_data) = mnist_loader.load_data_wrapper()
 
-nn = NeuralNetwork(784, 4, [1000, 2000, 1000, 10])
+# nn = NeuralNetwork(784, 3, [100, 100, 10], [Activation.LEAKY_RELU, Activation.LEAKY_RELU, Activation.SOFTMAX], learning_rate = 0.0005)
 
-for (i, data) in enumerate(training_data):
-    if i % 1000 == 0:
-        print(i)
-    (img, expected) = data
-    nn.fit(img / 255, expected)
+# training_data = list(training_data)
 
-errors = 0
+# for i in range(10):
+    # print("epoch", i + 1)
+    # for data in training_data:
+        # (img, expected) = data
+        # print(img)
+        # nn.fit(img, expected)
 
-for data in test_data:
-    (img, expected) = data
-    result = nn.predict(img / 255)
-    print(result, expected)
-    if np.linalg.norm(result - expected) > 0.1:
-        errors += 1
+# errors = 0
 
-print("errors: ", errors / 10000)
+# for data in test_data:
+    # (img, expected) = data
+    # result = nn.predict(img)
+    # if result.argmax() != expected:
+        # errors += 1
 
+# print("errors: ", errors / 10000)
+
+######
+
+# nn = NeuralNetwork(2, 2, [2, 4], [Activation.SIGMOID, Activation.SOFTMAX])
+# # nn = Layer(1, 2, Activation.SIGMOID, 1/1000)
+
+# # Function to classify points based on their position in the four smaller squares
+# def classify_square(x, y):
+    # if 0 <= x <= 0.5 and 0 <= y <= 0.5:
+        # return np.array([[1], [0], [0], [0]])  # Square 1
+    # else:
+        # return np.array([[0], [1], [0], [0]])  # Square 2
+    # # elif 0.5 < x <= 1 and 0 <= y <= 0.5:
+        # # return np.array([[0], [1], [0], [0]])  # Square 2
+    # # elif 0 <= x <= 0.5 and 0.5 < y <= 1:
+        # # return np.array([[0], [0], [1], [0]])  # Square 3
+    # # elif 0.5 < x <= 1 and 0.5 < y <= 1:
+        # # return np.array([[0], [0], [0], [1]])  # Square 4
+
+# # Generate random points in the unit square
+# num_points = 100000
+# points = np.random.rand(num_points, 2)  # Shape: (100000, 2)
+
+# # Classify points based on their position in the smaller squares
+# classifications = np.array([classify_square(x, y) for x, y in points])
+
+# points = points.reshape(num_points, 2, 1)
+
+# for i in range(num_points):
+    # # print("data:", points[i])
+    # nn.fit(points[i], classifications[i])
+    # # print("\n")
+
+# num_points = 100
+# points = np.random.rand(num_points, 2)  # Shape: (100000, 2)
+# points = points.reshape(num_points, 2, 1)
+
+# for i in range(num_points):
+    # print(points[i])
+    # print()
+    # print("expected: ", classify_square(points[i][0], points[i][1]))
+    # print("got:", nn.predict(points[i]))
+    # print("---")
+
+# print(nn.layers[0].weights)
+# print(nn.layers[1].weights)
+
+######
+
+nn = NeuralNetwork(2, 2, [2, 2], [Activation.SIGMOID, Activation.SOFTMAX], 0.1)
+nn.layers[0].weights = np.array([[0.1, -0.2, 0.3], [-0.4, 0.5, -0.6]])
+nn.layers[1].weights = np.array([[0.15, -0.25, 0.35], [-0.45, 0.55, -0.65]])
+
+nn.fit(np.array([[1], [0]]), np.array([[1], [0]]))
+print(nn.layers[0].weights)
+print(nn.layers[1].weights)
